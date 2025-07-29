3521a3604d44a0d955e31e083eea23e3240632cc
Caio-Lessaa <caio.lessa01@fatec.sp.gov.br>
2025-07-29 14:11:47 -0300

    Corrigindo erros metodo POST

--- aula-salvar-para-um/src/main/java/com/caioLessa/aula_salvar_para_um/controllers/PersonController.java
+++ aula-salvar-para-um/src/main/java/com/caioLessa/aula_salvar_para_um/controllers/PersonController.java
@@ -4,9 +4,7 @@ import com.caioLessa.aula_salvar_para_um.dto.PersonDepartmentDTO;
 import com.caioLessa.aula_salvar_para_um.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -18,7 +16,7 @@ public class PersonController {
     private PersonService personService;
 
     @PostMapping
-    public ResponseEntity<PersonDepartmentDTO> insert(PersonDepartmentDTO dto) {
+    public ResponseEntity<PersonDepartmentDTO> insert(@RequestBody PersonDepartmentDTO dto) {
         dto = personService.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
--- aula-salvar-para-um/src/main/java/com/caioLessa/aula_salvar_para_um/services/PersonService.java
+++ aula-salvar-para-um/src/main/java/com/caioLessa/aula_salvar_para_um/services/PersonService.java
@@ -19,8 +19,10 @@ public class PersonService {
         entity.setName(dto.getName());
         entity.setSalary(dto.getSalary());
         Department dept = new Department();
-        dept.setId(entity.getDepartment().getId());
+        System.out.println(dto.getDepartment().getId());
+        dept.setId(dto.getDepartment().getId());
         entity.setDepartment(dept);
+        entity = personRepository.save(entity);
         return new PersonDepartmentDTO(entity);
     }
 }
