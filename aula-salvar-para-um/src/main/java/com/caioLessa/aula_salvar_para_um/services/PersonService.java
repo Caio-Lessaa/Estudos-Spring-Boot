57d06b85a3cd6e78e657b6fc68514c6cc7844cc2
Caio-Lessaa <caio.lessa01@fatec.sp.gov.br>
2025-07-29 14:16:13 -0300

    Recuperando o nome do departamento para o POST de person

--- aula-salvar-para-um/src/main/java/com/caioLessa/aula_salvar_para_um/services/PersonService.java
+++ aula-salvar-para-um/src/main/java/com/caioLessa/aula_salvar_para_um/services/PersonService.java
@@ -3,24 +3,26 @@ package com.caioLessa.aula_salvar_para_um.services;
 import com.caioLessa.aula_salvar_para_um.dto.PersonDepartmentDTO;
 import com.caioLessa.aula_salvar_para_um.entities.Department;
 import com.caioLessa.aula_salvar_para_um.entities.Person;
+import com.caioLessa.aula_salvar_para_um.repositories.DepartmentRepository;
 import com.caioLessa.aula_salvar_para_um.repositories.PersonRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
     private PersonRepository personRepository;
+    private DepartmentRepository departmentRepository;
 
-    public PersonService(PersonRepository personRepository) {
+    public PersonService(PersonRepository personRepository, DepartmentRepository departmentRepository) {
         this.personRepository = personRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     public PersonDepartmentDTO insert(PersonDepartmentDTO dto) {
         Person entity = new Person();
         entity.setName(dto.getName());
         entity.setSalary(dto.getSalary());
-        Department dept = new Department();
-        System.out.println(dto.getDepartment().getId());
-        dept.setId(dto.getDepartment().getId());
+        Department dept = departmentRepository.getReferenceById(dto.getDepartment().getId());
+        
         entity.setDepartment(dept);
         entity = personRepository.save(entity);
         return new PersonDepartmentDTO(entity);
